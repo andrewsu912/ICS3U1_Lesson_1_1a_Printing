ccc3c4efac012e47db6ce6e5663a42c1de126b16
andrew.su912 <andrew.su912@gmail.com>
2024-02-08 11:40:09 -0500

    Updated PrintingComments.java

--- PrintingComments.java
+++ PrintingComments.java
@@ -8,14 +8,24 @@ class PrintingComments extends ConsoleProgram {
   public void run() {
 
     // Say hello
-    
+    System.out.println("Hi");
+    System.out.println("There!");
 
     // Say hello on the same line
-    
+    System.out.print("Hi");
+    System.out.print(" There!");
     
     // multiline comment
-    
-    
-
+    /*hi
+     i like noodles
+     kinda
+     */
+    // draw a box
+    System.out.println("*******");
+    System.out.println("*     *");
+    System.out.println("*     *");
+    System.out.println("*     *");
+    System.out.println("*     *");
+    System.out.println("*******");
   }
 }
